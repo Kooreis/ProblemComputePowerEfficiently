9d601b7ad6bf8dcb8d9dc442f812b8f9b7d7ad79
Kooreis <kooreis.official@gmail.com>
2024-08-30 14:40:15 -0500

    Created the Main class with the main method. Inside the main method, created a Scanner object to read the base number and exponent from the user. Then, called the power function with the base and exponent as arguments and printed the result.

--- solution.java
+++ solution.java
@@ -1,1 +1,9 @@
-import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter base number:");
+        double base = scanner.nextDouble();
+        System.out.println("Enter exponent:");
+        int exponent = scanner.nextInt();
+        System.out.println("Result: " + power(base, exponent));
+    }
