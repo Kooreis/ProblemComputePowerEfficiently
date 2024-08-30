ece1f5b1859adea41c704d7a0a2b3cd6ba0a299b
Kooreis <kooreis.official@gmail.com>
2024-08-30 14:40:15 -0500

    Added user input for base number and exponent.

--- solution.cs
+++ solution.cs
@@ -4,5 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.Write("Enter the base number: ");
+        double baseNumber = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the exponent: ");
+        int exponent = Convert.ToInt32(Console.ReadLine());
     }
 }
